2f126ac6f8c0ba324a5754615af3ffcab9131f36
j-leiyan <j-leiyan@microsoft.com>
2016-05-18 19:20:57 +0800

    Add test about DataTimeToUTC

--- Plot/UnitTest1.cs
+++ Plot/UnitTest1.cs
@@ -16,5 +16,29 @@ namespace Plot
 
             }
         }
+
+        [TestMethod]
+        public void TestLocalTimeToUTC()
+        {
+            DateTime now = DateTime.Now;
+            DateTime start = new DateTime(now.Year, now.Month, now.Day, 23, 00, 0);
+            DateTime end = new DateTime(now.Year, now.Month, now.Day, 3, 30, 0);
+            DateTime utcStart = start.ToUniversalTime();
+            DateTime utcEnd = end.ToUniversalTime();
+            string utcStartString = utcStart.ToString("hh:mm");
+            string utcEndString = utcEnd.ToString("hh:mm");
+        }
+
+        [TestMethod]
+        public void TestUTCTimeToLocal()
+        {
+            DateTime now = DateTime.Now;
+            DateTime start = new DateTime(now.Year, now.Month, now.Day, 23, 00, 0, DateTimeKind.Utc);
+            DateTime end = new DateTime(now.Year, now.Month, now.Day + 1, 3, 30, 0, DateTimeKind.Utc);
+            DateTime localStart = start.ToLocalTime();
+            DateTime localEnd = end.ToLocalTime();
+            string localStartString = localStart.ToString("hh:mm");
+            string localEndString = localEnd.ToString("hh:mm");
+        }
     }
 }
